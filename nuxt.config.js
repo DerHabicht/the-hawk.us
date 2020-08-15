527caabef491aa84b705c0ccdfdae63f653e9a96
Robert Hawk <robert@the-hawk.us>
2020-08-15 06:36:52 -0600

    feat: Initial layouts
    Created a placeholder landing page and the Starcrest bulletin redirect.

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,13 +21,35 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'msapplication-TileColor', content: '#2b5797' },
+      { name: 'theme-color', content: '#ffffff' },
       {
         hid: 'description',
         name: 'description',
         content: process.env.npm_package_description || '',
       },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'manifest', href: '/site.webmanifest' },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#0d2b99' },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon-16x16.png',
+      },
+    ],
   },
   /*
    ** Global CSS
---
+++ pages/starcrest.vue
@@ -0,0 +1,13 @@
+<template><div></div></template>
+
+<script>
+export default {
+  name: 'Starcrest',
+  mounted() {
+    // eslint-disable-next-line nuxt/no-globals-in-created
+    window.location.replace(
+      'https://drive.google.com/file/d/0B0paKDvkD_ruZkZIWTY2X1YxZnM'
+    )
+  },
+}
+</script>
