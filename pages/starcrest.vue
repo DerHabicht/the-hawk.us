ece1746c7c39535b7766615547e9d52502fa602d
Robert Hawk <robert.hawk@unitedmasters.com>
2021-08-01 11:29:23 -0600

    chore: Update share link for Starcrest bulletin
    With the new security updates to Google Drive, share links for documents
    have changed. This commit updates the Starcrest bulletin link to the
    correct share link.

--- pages/starcrest.vue
+++ pages/starcrest.vue
@@ -6,7 +6,7 @@ export default {
   mounted() {
     // eslint-disable-next-line nuxt/no-globals-in-created
     window.location.replace(
-      'https://drive.google.com/file/d/0B0paKDvkD_ruZkZIWTY2X1YxZnM'
+      'https://drive.google.com/file/d/0B0paKDvkD_ruZkZIWTY2X1YxZnM/view?usp=sharing&resourcekey=0-fiuVT-TEIMQlge8ioB10jQ'
     )
   },
 }
